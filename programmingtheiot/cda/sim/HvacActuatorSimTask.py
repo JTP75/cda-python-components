153a75d149e088e6c6c1b9ec3253bc06e52c5ce1
Justin Pacella <jtp75@pitt.edu>
2025-09-24 12:28:39 -0400

    finished and tested actuators, all tests in  directory now pass :). finished cda-03-005. NOTE no errors from the previous commit for now...

--- programmingtheiot/cda/sim/HvacActuatorSimTask.py
+++ programmingtheiot/cda/sim/HvacActuatorSimTask.py
@@ -12,15 +12,20 @@
 import logging
 import random
 
+import programmingtheiot.common.ConfigConst as ConfigConst
+
 from programmingtheiot.data.ActuatorData import ActuatorData
 from programmingtheiot.cda.sim.BaseActuatorSimTask import BaseActuatorSimTask
 
 class HvacActuatorSimTask(BaseActuatorSimTask):
-	"""
-	Shell representation of class for student implementation.
-	
-	"""
+    """
+    Actuator simulation task for HVAC system. Simulation for testing and development purposes.
+    """
 
-	def __init__(self):
-		pass
-		
+    def __init__(self):
+        super(HvacActuatorSimTask, self).__init__(
+            name=ConfigConst.HVAC_ACTUATOR_NAME,
+            typeID=ConfigConst.HVAC_ACTUATOR_TYPE,
+            simpleName="HVAC"
+        )
+        
